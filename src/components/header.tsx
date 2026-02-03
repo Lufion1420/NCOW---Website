3f11fb1916155f82edf51d9ac8f776fa0ae25b6b
Maurice - TPG <m.samba@the-platform-group.com>
2026-02-03 22:35:19 +0100

    added basic coloring for otsusuki theme + started header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,50 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
-  return <header className="header">This is the Header</header>;
+  const mainLinks = {};
+
+  return (
+    <header className="header">
+      <h1 className="logo">
+        <Link to={`/`}>
+          <img src="https://placehold.co/250x150" alt="NCOW - Logo" />
+        </Link>
+      </h1>
+      <nav className="main-nav">
+        <ul>
+          <li>
+            <Link to={`/`}>Gameplay</Link>
+          </li>
+          <li>
+            <Link to={`/`}>Items</Link>
+          </li>
+          <li>
+            <Link to={`/`}>Terrain</Link>
+          </li>
+          <li>
+            <Link to={`/`}>Downloads</Link>
+          </li>
+          <li>
+            <Link to={`/`}>Guides</Link>
+          </li>
+          <li>
+            <Link to={`/`}>Roadmap</Link>
+          </li>
+        </ul>
+      </nav>
+      <nav className="sub-nav">
+        <ul>
+          <li>
+            <Link to={`/`}>Login</Link>
+          </li>
+          <li>
+            <Link to={`/`}>Donations</Link>
+          </li>
+          <li>
+            <Link to={`/`}>PLAY HERE</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
