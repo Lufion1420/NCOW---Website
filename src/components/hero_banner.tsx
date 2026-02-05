142ede64cb8e5420395df277abee9e1ba08d8fab
Maurice - TPG <m.samba@the-platform-group.com>
2026-02-05 20:35:50 +0100

    banner switched

--- src/components/hero_banner.tsx
+++ src/components/hero_banner.tsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import "../styles/hero_banner.css";
+import wallpaper from "../assets/ui/NCOW-Logo-1.png";
 
 export default function HeroBanner() {
   return (
     <div className="hero_banner">
-      <img src="../assets/wallpapers/NCOW-Wallpaper-3.png" alt="Teste" />
+      <img src={wallpaper} alt="Teste" />
     </div>
   );
 }
