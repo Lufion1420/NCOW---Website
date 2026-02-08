84e70380cb7ab3027ab07e393f54e74bd5276330
Maurice - TPG <m.samba@the-platform-group.com>
2026-02-08 18:16:56 +0100

    WIP Quick Banner

---
+++ src/components/quick_banner.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "../styles/quick_banner.css";
+import Arrow from "../assets/ui/NCOW-Kunai-3.png";
+
+export default function QuickBanner() {
+  return (
+    <div className="quick_banner">
+      <div className="quick_banner_wrapper">
+        <div className="quick_banner_content container">
+          <Link to="/XXX"></Link>
+          <h3>Jetzt vorbestellen</h3>
+          <div className="quick_banner_subheading">
+            <span>Dekorüstung für Eleanor: Himmelschuppenkönigin</span>
+          </div>
+          <div className="quick_banner_text">
+            <span className="glow_effect-secondary">Vorgestellbonus sichern</span>
+            <img src={Arrow} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,10 +1,12 @@
 import HeroBanner from "../components/hero_banner";
 import IntroBanner from "../components/intro_banner";
+import QuickBanner from "../components/quick_banner";
 
 export default function HomePage() {
   return (
     <>
       <HeroBanner></HeroBanner>
+      <QuickBanner></QuickBanner>
       <IntroBanner></IntroBanner>
     </>
   );
