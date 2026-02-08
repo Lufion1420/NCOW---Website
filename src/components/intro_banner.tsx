7d70c32425f04aff1a054ddc56ea456b9cf673f6
Maurice - TPG <m.samba@the-platform-group.com>
2026-02-09 00:46:23 +0100

    WIP - Intro Banner Rework

--- src/components/intro_banner.tsx
+++ src/components/intro_banner.tsx
@@ -1,5 +1,4 @@
 import "../styles/intro_banner.css";
-import Image from "../assets/ui/NCOW-Juubi.png";
 import Arrow from "../assets/ui/NCOW-Kunai-3.png";
 import { Link } from "react-router-dom";
 
@@ -8,13 +7,17 @@ export default function IntroBanner() {
     <div className="intro_banner container">
       <h2>Experience a Naruto RPG like never seen before!</h2>
 
-      <div className="intro_text">
-        <p>Naruto Clash of Worlds is a custom Warcraft III mod that lets you experience the Naruto story as a full-scale RPG. Choose one of four factions, Akatsuki, Konoha, Otogakure, or Evil and dive into story-driven gameplay where you define your role. Featuring custom 3D models, quests, UI, and skillsets, this is the spiritual successor to Naruto World, but bigger, deeper, and more ambitious than ever.</p>
-        <button>
-          <Link to={"/gameplay"}></Link>
-          <span>About Naruto Clash of Worlds</span>
-          <img src={Arrow} alt="Arrow Right" />
-        </button>
+      <div className="intro_inner_wrap">
+        <div className="intro_video"></div>
+
+        <div className="intro_text">
+          <p>Naruto Clash of Worlds is a custom Warcraft III mod that lets you experience the Naruto story as a full-scale RPG. Choose one of four factions, Akatsuki, Konoha, Otogakure, or Evil and dive into story-driven gameplay where you define your role. Featuring custom 3D models, quests, UI, and skillsets, this is the spiritual successor to Naruto World, but bigger, deeper, and more ambitious than ever.</p>
+          <button>
+            <Link to={"/gameplay"}></Link>
+            <span>About Naruto Clash of Worlds</span>
+            <img src={Arrow} alt="Arrow Right" />
+          </button>
+        </div>
       </div>
     </div>
   );
