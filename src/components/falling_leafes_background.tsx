dc786e8f6503c147dbc1f91e96f4df959295a640
Maurice - TPG <m.samba@the-platform-group.com>
2026-02-08 02:42:01 +0100

    Finished falling leafs component and integrated it into the Hero Banner

--- src/components/falling_leafes_background.tsx
+++ src/components/falling_leafes_background.tsx
@@ -3,31 +3,87 @@ import "../styles/falling_leaf.css";
 
 export default function FallingLeafs() {
   return (
-    <div className="falling_leafs">
-      <div>
-        <img src={leaf} alt="Leaf" />
+    <>
+      <div className="falling_leafs">
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
       </div>
-      <div>
-        <img src={leaf} alt="Leaf" />
+
+      <div className="falling_leafs set2">
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
       </div>
-      <div>
-        <img src={leaf} alt="Leaf" />
+
+      <div className="falling_leafs set3">
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
+        <div>
+          <img src={leaf} alt="Leaf" />
+        </div>
       </div>
-      <div>
-        <img src={leaf} alt="Leaf" />
-      </div>
-      <div>
-        <img src={leaf} alt="Leaf" />
-      </div>
-      <div>
-        <img src={leaf} alt="Leaf" />
-      </div>
-      <div>
-        <img src={leaf} alt="Leaf" />
-      </div>
-      <div>
-        <img src={leaf} alt="Leaf" />
-      </div>
-    </div>
+    </>
   );
 }
