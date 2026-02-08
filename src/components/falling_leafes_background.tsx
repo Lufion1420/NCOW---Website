2ef06e308f57d30d7d65db2f320e755917c0a8df
Maurice - TPG <m.samba@the-platform-group.com>
2026-02-08 02:35:43 +0100

    WIP Falling Leafs component

---
+++ src/components/falling_leafes_background.tsx
@@ -0,0 +1,33 @@
+import leaf from "../assets/ui/NCOW-Leaf.png";
+import "../styles/falling_leaf.css";
+
+export default function FallingLeafs() {
+  return (
+    <div className="falling_leafs">
+      <div>
+        <img src={leaf} alt="Leaf" />
+      </div>
+      <div>
+        <img src={leaf} alt="Leaf" />
+      </div>
+      <div>
+        <img src={leaf} alt="Leaf" />
+      </div>
+      <div>
+        <img src={leaf} alt="Leaf" />
+      </div>
+      <div>
+        <img src={leaf} alt="Leaf" />
+      </div>
+      <div>
+        <img src={leaf} alt="Leaf" />
+      </div>
+      <div>
+        <img src={leaf} alt="Leaf" />
+      </div>
+      <div>
+        <img src={leaf} alt="Leaf" />
+      </div>
+    </div>
+  );
+}
--- src/components/hero_banner.tsx
+++ src/components/hero_banner.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import "../styles/hero_banner.css";
 import logo from "../assets/logos/NCOW-Logo-1.png";
+import FallingLeafs from "../components/falling_leafes_background";
 
 export default function HeroBanner() {
   return (
     <div className="hero_banner">
       <img src={logo} alt="NCOW Logo" />
       <span>Coming Soon!</span>
+      <FallingLeafs></FallingLeafs>
     </div>
   );
 }
