b2632f6b5d6abb50821fecfcff2095078eead1e9
Maurice - TPG <m.samba@the-platform-group.com>
2026-02-09 02:52:09 +0100

    WIP - Character Cards Component

--- src/components/character_cards.tsx
+++ src/components/character_cards.tsx
@@ -1,11 +1,15 @@
+import { useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import "swiper/swiper.css";
+
 import "../styles/character_cards.css";
 
 import Naruto from "../assets/characters/NCOW_IMG_NarutoBaseSelect.png";
 import Icon_Naruto from "../assets/characters/NCOW_IMG_NarutoBase.png";
+import Naruto_Image_1 from "../assets/characters/NCOW-Placeholder-1.png";
+import Naruto_Image_2 from "../assets/characters/NCOW-Placeholder-2.png";
 
 import Sasuke from "../assets/characters/NCOW_IMG_SasukeBaseSelect.png";
 import Icon_Sasuke from "../assets/characters/NCOW_IMG_SasukeHebi.png";
@@ -17,71 +21,104 @@ import Obito from "../assets/characters/NCOW_IMG_ObitoMaskedManSelect.png";
 import Icon_Obito from "../assets/characters/NCOW_IMG_MaskedMan.png";
 
 export default function CharacterCards() {
-  const characters = [
-    {
-      naruto: {
+  const characters = useMemo(
+    () => [
+      {
+        id: "naruto",
         name: "Naruto Uzumaki",
-        title: "Kyuubi Jinjuriki",
-        image_main: { Naruto },
-        icon: { Icon_Naruto },
-        description: "Naruto Description here1",
+        title: "Kyuubi Jinchuriki",
+        image_main: Naruto,
+        icon: Icon_Naruto,
+        description: "Der Prinz (oder die Prinzessin) von Azuria und Kapitän der Rangers. Sie sind äußerst geschickt und die einzigen Rathalos-Rider im Land. Der Protagonist hat sich auf seine Pflichten als Ranger konzentriert, indem er das Ökosystem der Monster beschützt und den Ei-Quarz erforscht, als ein schockierendes Ereignis alles verändert. Sie stellen fest, dass sie nur wenig bis gar nichts über die Ereignisse in der Welt und die Beteiligung der Menschen daran wissen. Um die Wahrheit zu erfahren, machen sie sich auf den Weg über den Nord-Meridian und auf verbotenen Boden.",
         link: "/XXX",
+        char_images: [Naruto_Image_1, Naruto_Image_2] as string[],
       },
-      sasuke: {
+      {
+        id: "sasuke",
         name: "Sasuke Uchiha",
         title: "Uchiha Avenger",
-        image: { Sasuke },
-        icon: { Icon_Sasuke },
-        description: "Naruto Description here",
+        image_main: Sasuke,
+        icon: Icon_Sasuke,
+        description: "Sasuke description here.",
         link: "/XXX",
+        char_images: [] as string[],
       },
-      itachi: {
-        name: " Itachi Uchiha",
+      {
+        id: "itachi",
+        name: "Itachi Uchiha",
         title: "Uchiha Traitor",
-        image_main: { Itachi },
-        icon: { Icon_Itachi },
-        description: "Naruto Description here",
+        image_main: Itachi,
+        icon: Icon_Itachi,
+        description: "Itachi description here.",
         link: "/XXX",
+        char_images: [] as string[],
       },
-      obito: {
+      {
+        id: "obito",
         name: "Obito Uchiha",
         title: "Masked Man",
-        image_main: { Obito },
-        icon: { Icon_Obito },
-        description: "Naruto Description here",
+        image_main: Obito,
+        icon: Icon_Obito,
+        description: "Obito description here.",
         link: "/XXX",
+        char_images: [] as string[],
       },
-    },
-  ];
+    ],
+    [],
+  );
+
+  const [selectedCharacterId, setSelectedCharacterId] = useState<string>(characters[0].id);
+
+  const selectedCharacter = characters.find((character) => character.id === selectedCharacterId) ?? characters[0];
 
   return (
-    <div className="character_cards container">
-      <div className="char_wrapper">
-        <div className="char_content">
-          <div className="char_name"></div>
-          <div className="char_title"></div>
-          <div className="char_description"></div>
-          <div className="char_images">
-            <div className="char_image"></div>
-            <div className="char_image"></div>
+    <>
+      <div className="character_cards container">
+        <div className="char_wrapper">
+          <div className="char_content">
+            <span className="char_name">{selectedCharacter.name}</span>
+            <span className="char_title">{selectedCharacter.title}</span>
+            <p className="char_description">{selectedCharacter.description}</p>
+            <div className="char_images">
+              <Swiper modules={[Navigation, Pagination]} slidesPerView={1} spaceBetween={12} navigation pagination={{ clickable: true }}>
+                {selectedCharacter.char_images.length > 0 ? (
+                  selectedCharacter.char_images.map((charImage, index) => (
+                    <SwiperSlide key={`${selectedCharacter.id}-image-${index}`}>
+                      <div className="char_image">
+                        <img src={charImage} alt={`${selectedCharacter.name} screenshot ${index + 1}`} />
+                      </div>
+                    </SwiperSlide>
+                  ))
+                ) : (
+                  <SwiperSlide key={`${selectedCharacter.id}-image-empty`}>
+                    <div className="char_image char_image-empty">No additional images yet.</div>
+                  </SwiperSlide>
+                )}
+              </Swiper>
+            </div>
+            <button>
+              <span>More Info</span>
+              <Link to={selectedCharacter.link}></Link>
+            </button>
           </div>
-          <button>
-            <a href=""></a>
-          </button>
-        </div>
 
-        <div className="char_main_image">
-          <img src="" alt="Main Character Image" />
+          <div className="char_main_image">
+            <img src={selectedCharacter.image_main} alt={selectedCharacter.name} />
+          </div>
         </div>
       </div>
 
-      <ul className="char_list">
-        <li>
-          <button>
-            <img src="" alt="Icon Character" />
-          </button>
-        </li>
-      </ul>
-    </div>
+      <div className="char_list">
+        <Swiper modules={[Navigation]} tag="ul" slidesPerView={4} spaceBetween={10} navigation breakpoints={{ 0: { slidesPerView: 3 }, 768: { slidesPerView: 4 }, 1024: { slidesPerView: 6 } }}>
+          {characters.map((character) => (
+            <SwiperSlide key={character.id} tag="li">
+              <button className={`char_icon_button ${selectedCharacter.id === character.id ? "is-active" : ""}`} onClick={() => setSelectedCharacterId(character.id)} aria-label={`Select ${character.name}`}>
+                <img src={character.icon} alt={`${character.name} icon`} />
+              </button>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
   );
 }
