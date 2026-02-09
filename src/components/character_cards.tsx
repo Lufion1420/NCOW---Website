baa7aa4c32e1af6525694762cb50ef06360cdd7a
Maurice - TPG <m.samba@the-platform-group.com>
2026-02-09 20:10:35 +0100

    WIP CSS updates

--- src/components/character_cards.tsx
+++ src/components/character_cards.tsx
@@ -1,4 +1,6 @@
 import "../styles/character_cards.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.css";
 
 import ImageNaruto from "../assets/characters/NCOW_IMG_NarutoBaseXL.webp";
 
@@ -8,38 +10,44 @@ export default function CharacterCards() {
       naruto: {
         image: ImageNaruto,
         name: "Naruzo Uzumaki",
-        description: "Naruto Clash of Worlds is a custom Warcraft III mod that lets you experience the Naruto story as a full-scale RPG. Choose one of four factions, Akatsuki, Konoha, Otogakure, or Evil and dive into story-driven gameplay where you define your role. Featuring custom 3D models, quests, UI, and skillsets, this is the spiritual successor to Naruto World, but bigger, deeper, and more ambitious than ever.",
+        description: "Naruto Clash of Worlds is a custom Warcraft III mod that lets you experience the Naruto story as a full-scale RPG. Choose one of four factions, Akatsuki, Konoha, Otogakure, or Evil and dive into story-driven gameplay where you define your role. .",
       },
       sasuke: {
         image: ImageNaruto,
         name: "Sasuke Uchiha",
-        description: "Naruto Clash of Worlds is a custom Warcraft III mod that lets you experience the Naruto story as a full-scale RPG. Choose one of four factions, Akatsuki, Konoha, Otogakure, or Evil and dive into story-driven gameplay where you define your role. Featuring custom 3D models, quests, UI, and skillsets, this is the spiritual successor to Naruto World, but bigger, deeper, and more ambitious than ever.",
+        description: "Naruto Clash of Worlds is a custom Warcraft III mod that lets you experience the Naruto story as a full-scale RPG. Choose one of four factions, Akatsuki, Konoha, Otogakure, or Evil and dive into story-driven gameplay where you define your role. .",
       },
       itachi: {
         image: ImageNaruto,
         name: "Itachi Uchiha",
-        description: "Naruto Clash of Worlds is a custom Warcraft III mod that lets you experience the Naruto story as a full-scale RPG. Choose one of four factions, Akatsuki, Konoha, Otogakure, or Evil and dive into story-driven gameplay where you define your role. Featuring custom 3D models, quests, UI, and skillsets, this is the spiritual successor to Naruto World, but bigger, deeper, and more ambitious than ever.",
+        description: "Naruto Clash of Worlds is a custom Warcraft III mod that lets you experience the Naruto story as a full-scale RPG. Choose one of four factions, Akatsuki, Konoha, Otogakure, or Evil and dive into story-driven gameplay where you define your role. .",
       },
       obito: {
         image: ImageNaruto,
         name: "Obito Uchiha",
-        description: "Naruto Clash of Worlds is a custom Warcraft III mod that lets you experience the Naruto story as a full-scale RPG. Choose one of four factions, Akatsuki, Konoha, Otogakure, or Evil and dive into story-driven gameplay where you define your role. Featuring custom 3D models, quests, UI, and skillsets, this is the spiritual successor to Naruto World, but bigger, deeper, and more ambitious than ever.",
+        description: "Naruto Clash of Worlds is a custom Warcraft III mod that lets you experience the Naruto story as a full-scale RPG. Choose one of four factions, Akatsuki, Konoha, Otogakure, or Evil and dive into story-driven gameplay where you define your role. .",
       },
     },
   ];
 
+  const characterList = Object.values(characters[0]);
+
   return (
     <div className="character_cards">
       <h2>Characters</h2>
-      <div className="card_list">
-        <div className="card">
-          <img src="" alt="" />
-          <div className="card_content">
-            <div className="card_name"></div>
-            <div className="card_description"></div>
-          </div>
-        </div>
-      </div>
+      <Swiper className="card_list" slidesPerView={5} centeredSlides spaceBetween={24} watchOverflow>
+        {characterList.map((character) => (
+          <SwiperSlide key={character.name}>
+            <article className="card">
+              <img src={character.image} alt={character.name} />
+              <div className="card_content">
+                <div className="card_name">{character.name}</div>
+                <div className="card_description">{character.description}</div>
+              </div>
+            </article>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
