be328d5ec9396e527026854565a448b7ec55538d
Maurice - TPG <m.samba@the-platform-group.com>
2026-02-10 12:48:19 +0100

    small adjustments

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -2,19 +2,12 @@ import HeroBanner from "../components/hero_banner";
 import IntroBanner from "../components/intro_banner";
 import QuickBanner from "../components/quick_banner";
 import CharacterCards from "../components/character_cards";
-import Juubi from "../assets/ui/NCOW-Juubi.png";
 
 export default function HomePage() {
   return (
     <>
       <HeroBanner></HeroBanner>
-      <QuickBanner
-        imageSrc={Juubi}
-        linkTo="/XXX"
-        headingText="Jetzt vorbestellen"
-        subheadingText="Dekorüstung für Eleanor: Himmelschuppenkönigin"
-        quickBannerText="Vorgestellbonus sichern"
-      ></QuickBanner>
+      <QuickBanner linkTo="/XXX" headingText="Jetzt vorbestellen" subheadingText="Dekorüstung für Eleanor: Himmelschuppenkönigin" quickBannerText="Vorgestellbonus sichern"></QuickBanner>
       <IntroBanner></IntroBanner>
       <CharacterCards></CharacterCards>
     </>
