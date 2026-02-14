3fbc04db14441e54ccd1359c343596f1cd88b4e7
Maurice - TPG <m.samba@the-platform-group.com>
2026-02-14 20:11:29 +0100

    WIP Gameplay Features component.

--- src/components/gameplay_features.tsx
+++ src/components/gameplay_features.tsx
@@ -6,15 +6,47 @@ import "../styles/gameplay_features.css";
     --> Switching layout from Image left and text right to text left and image right every second sub area.
 */
 
-function Feature() {
-  return <div className="feature"></div>;
+type Alignment = "left" | "right";
+type Props = {
+  heading?: string;
+  text?: string;
+  alignment?: Alignment;
+};
+
+function Feature({ heading, text, alignment }: Props) {
+  return (
+    <div className={`feature pseudo alignment-${alignment}`}>
+      <div className="title">
+        <h2>{heading}</h2>
+      </div>
+      <div className="text">
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 }
+
+const content = {
+  Quests: {
+    heading: "Quests",
+    text: "This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text!",
+  },
+
+  Missions: {
+    heading: "Missions",
+    text: "This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text! This could be your text!",
+  },
+};
+
+const list_content = Object.values(content);
 
 export default function GameplayFeatures() {
   return (
-    <div className="gameplay_features container">
+    <div className="gameplay_features">
       <div className="pseudo wrapper">
-        <Feature></Feature>
+        {list_content.map((heading: string, text: string, alignment: Alignment) => (
+          <Feature heading={heading} text={text} alignment={alignment}></Feature>
+        ))}
       </div>
     </div>
   );
