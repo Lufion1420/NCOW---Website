f4ed8425cb6a153abf130997701080ec47a3856f
Maurice - TPG <m.samba@the-platform-group.com>
2026-02-16 16:13:19 +0100

    ignore

--- src/pages/items.tsx
+++ src/pages/items.tsx
@@ -1,8 +1,11 @@
 import ItemIntro from "../components/item_intro";
+import PageBanner from "../components/page_banner";
+import ImageBanner from "../assets/ui/NCOW-Banner-5.png";
 
 export default function Items() {
   return (
     <div>
+      <PageBanner image={ImageBanner} heading="Items"></PageBanner>
       <ItemIntro></ItemIntro>
     </div>
   );
